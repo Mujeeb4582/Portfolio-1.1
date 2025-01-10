0c411cf4c965eab64ee01607b9815b2fda52ec83
Mujeeb4582 <mujeeburahman4582@gmail.com>
2025-01-10 11:55:35 +0500

    add test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AboutSection from '@/app/ui/homepage/aboutSection'
 import HeroSection from '@/app/ui/homepage/heroSection'
 
 export default function Home() {
+  console.log('homepage')
   return (
     <main className="flex w-full flex-col items-center justify-center">
       <HeroSection />

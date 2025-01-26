80c953f521845e2d55fdc088a25e114d687ba821
Mujeeb4582 <mujeeburahman4582@gmail.com>
2025-01-26 22:46:17 +0500

    Add UnderLine component for visual separation in UI

---
+++ app/ui/underLine.tsx
@@ -0,0 +1,9 @@
+export function UnderLine() {
+  return (
+    <div className="mt-4 flex w-full items-center">
+      <div className="size-2 rounded-full bg-brand1"></div>
+      <div className="w-full border-t-2 border-brand1"></div>
+      <div className="size-2 rounded-full bg-brand1"></div>
+    </div>
+  )
+}
